75083ed76fe2e63dbb396209449018468e958a0b
Goncharenko Valery <goncharikvv@gmail.com>
2020-03-04 23:50:42 +0200

    5.8. JavaDoc. Code convention[#241794]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,17 +1,35 @@
 package ru.job4j.calculator;
-
+/**
+ * Класс Calculator: +,-,/,*.
+ * @author Valera Goncharenko
+ * @version 1
+ * @since 20.05.2019
+ */
 public class Calculator {
 
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
+        /**
+         * Contains numbers to calculate.
+         */
         int one = 1;
         int two = 2;
         int six = 6;
         int four = 4;
         int five = 5;
+        /**
+         * Contains numbers to +,-,/,*.
+         */
         int onePlusTwo = one + two;
         int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
+        /**
+         * line output to the console.
+         */
         System.out.println(onePlusTwo);
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculator task.
+ * @author Valera Goncharenko (goncharikvv@gmail.com)
+ * @version $Id$
+ * @since 4.03.20
+ */
+package ru.job4j.calculator;
