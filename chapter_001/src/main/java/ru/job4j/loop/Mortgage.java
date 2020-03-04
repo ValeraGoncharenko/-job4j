c10a59e52853bb24d631b5bc562ee9e5f1804df2
Goncharenko Valery <goncharikvv@gmail.com>
2020-03-04 23:24:07 +0200

    5.7. Ипотека.[#241786]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        double tmp = amount;
+        while (tmp > 0){
+            tmp = tmp + percent / 100 * amount - salary;
+            year++;
+        }
+        return year;
+    }
+}
