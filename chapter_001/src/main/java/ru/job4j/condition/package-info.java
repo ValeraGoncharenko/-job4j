4f43d8b5238034a0efa5c8656a0a013fe9111e36
Goncharenko Valery <goncharikvv@gmail.com>
2020-03-22 01:44:34 +0200

    2.2. Рефакторинг - Площадь треугольника.[#241880]

---
+++ chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for condition task.
+ *
+ * @author Valera Goncharenko (goncharikvv@gmail.com)
+ * @version $Id$
+ * @since 22.03.2020.
+ */
+package ru.job4j.condition;
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,14 +1,29 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
+/**Вычисление площади треугольника Test.
+ * @author Valera Goncharenko (goncharikvv@gmail.com).
+ * @version $Id$.
+ * @since 22.03.2020.
+ */
 public class TriangleTest {
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 2);
+        Point third = new Point(2, 0);
+        // Создаем объект треугольник.
+        Triangle triangle = new Triangle(first, second, third);
+        // Вычисляем площадь.
+        double result = triangle.area();
+        // Задаем ожидаемый результат.
+        double expected = 2;
+        //Проверяем результат и ожидаемое значение.
+        assertThat(result, closeTo(expected, 0.1));
     }
 
     @Test
