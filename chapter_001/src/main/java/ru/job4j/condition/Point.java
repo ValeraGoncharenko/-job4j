8ccd75526bd0ecbe985d98f99073e1aa64abf291
Goncharenko Valery <goncharikvv@gmail.com>
2020-03-22 02:34:47 +0200

    4. Расстояние между точками в трехмерном пространстве.[#241881]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,17 +1,61 @@
 package ru.job4j.condition;
 
+/**
+ * pасcтояние методу точками в системе координат.
+ * @author Valera Goncharenko
+ * @version $Id$
+ * @since 22.03.2020.
+ */
 public class Point {
+    /**
+     * Это поля объекта. Они доступно только конкретному объекту.
+     */
     private int x;
     private int y;
-
+    private int z;
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     * @param first координата x
+     * @param second координата y
+     */
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * Метод вычисляет расстояние методу точками в системе координат .
+     * @param  that точки в системе координат.
+     * double first-возводим числа во 2 степень.
+     * double second-возводим числа во 2 степень.
+     * @return расстояние.
+     */
     public double distance(Point that) {
         double first = Math.pow(this.x - that.x, 2);
         double second = Math.pow(this.y - that.y, 2);
         double rsl = Math.sqrt(first + second);
+        return rsl;
+    }
+
+    /**
+     * Метод вычисляет расстояние между точками в системе координат 3D.
+     * @param  that точки в системе координат.
+     * double first-возводим числа во 2 степень.
+     * double second-возводим числа во 2 степень.
+     * double third-возводим числа во 2 степень.
+     * @return расстояние.
+     */
+    public double distance3d(Point that) {
+        double first = Math.pow(this.x - that.x, 2);
+        double second = Math.pow(this.y - that.y, 2);
+        double third = Math.pow(this.z - that.z, 2);
+        double rsl = Math.sqrt(first + second + third);
         return rsl;
     }
 
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -3,14 +3,36 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-
+/**
+ * TestPoint.
+ *
+ * @author Valera Goncharenko (goncharikvv@gmail.com)
+ * @version $Id$
+ * @since 22.03.2020.
+ */
 public class PointTest {
+
+    /**
+     * Test for 2D.
+     */
     @Test
     public void distanceTest() {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
         double expected = 2;
         double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    /**
+     * Test for 3D.
+     */
+    @Test
+    public void distance3d() {
+        Point first = new Point(20, 10, 10);
+        Point second = new Point(40, 10,10);
+        double expected = 20;
+        double out = first.distance3d(second);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
