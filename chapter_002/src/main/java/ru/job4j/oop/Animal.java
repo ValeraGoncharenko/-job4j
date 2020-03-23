7f0a568fff340d349dd10f34e89dcb27d64a2a05
Goncharenko Valery <goncharikvv@gmail.com>
2020-03-23 21:17:28 +0200

    1. Конструктор по умолчанию.[#241894]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Animal {
+    public Animal(){
+        super();
+        System.out.println("Animal");
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Predator predator = new Predator();
+        Tiger tiger = new Tiger();
+    }
+
+}
