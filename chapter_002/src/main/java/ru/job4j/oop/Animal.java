f2259e2ff63041c6406e39b0683f77ab21b81853
Goncharenko Valery <goncharikvv@gmail.com>
2020-03-24 00:07:15 +0200

    2. Перегрузка конструктора.[#241895]

--- chapter_002/src/main/java/ru/job4j/oop/Animal.java
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -1,15 +1,26 @@
 package ru.job4j.oop;
 
 public class Animal {
+    String name;
+
     public Animal(){
         super();
         System.out.println("Animal");
+    }
+
+    public Animal(String name) {
+        this.name = name;
+        System.out.println(this.name);
     }
 
     public static void main(String[] args) {
         Animal animal = new Animal();
         Predator predator = new Predator();
         Tiger tiger = new Tiger();
+
+        Animal animalName = new Animal("animalName");
+        Predator predatorName = new Predator("predatorName");
+        Tiger tigerName = new Tiger("tigerName");
     }
 
 }
