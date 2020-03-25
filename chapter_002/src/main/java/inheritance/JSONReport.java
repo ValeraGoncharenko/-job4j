49e93e0f33b4c63ae16b1e518f5602a1d9227563
Goncharenko Valery <goncharikvv@gmail.com>
2020-03-26 01:06:32 +0200

    5. Аннотация @Override[#241897]

--- chapter_002/src/main/java/inheritance/JSONReport.java
+++ chapter_002/src/main/java/inheritance/JSONReport.java
@@ -2,6 +2,7 @@ package inheritance;
 
 public class JSONReport extends TextReport {
 
+    @Override
     public String generate(String name, String body) {
         return "{\n\n  name : " + name + ",\n  body : " + body + "\n\n}";
 
