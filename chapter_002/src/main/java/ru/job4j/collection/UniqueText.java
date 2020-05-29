080d4f0eac092bd748045e1233e17b08653ccaf5
Goncharenko Valery <goncharikvv@gmail.com>
2020-05-30 00:36:32 +0300

    2. Идентичные тексты.[212655#241831]

---
+++ chapter_002/src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        // for-each origin -> new HashSet.
+        for (String symbolOrigin : origin) {
+            check.add(symbolOrigin);
+        }
+        // for-each text -> hashSet.contains
+        for (String symbol : text) {
+            if (!check.contains(symbol)) {
+                rsl = false;
+            }
+
+        }
+        return rsl;
+    }
+}
