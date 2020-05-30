48d85f9ad3b4dd5ae8975a3e75365d14c5d95ede
Goncharenko Valery <goncharikvv@gmail.com>
2020-05-31 02:41:23 +0300

    1. Паспорт и жители.[212656#241826]

---
+++ chapter_002/src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void noAdd() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Valera");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        assertThat(office.add(citizen2), is(false));
+    }
+}
