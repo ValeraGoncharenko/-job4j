9cca1865fdacc331975edb6475fc79ded2e17eac
Goncharenko Valery <goncharikvv@gmail.com>
2020-08-23 16:21:47 +0300

    1. Collection API Улучшения[70623#241684]

--- chapter_002/src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -11,23 +11,23 @@ import static org.junit.Assert.assertThat;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1", "k1/sk1/ssk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1");
+        List<String> input = List.of("k1/sk1", "k1/sk1/ssk1");
+        List<String> expect = List.of("k1", "k1/sk1", "k1/sk1/ssk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenDepartmentsSortDesc() {
-        List<String> deps = Arrays.asList(
+        List<String> deps = List.of(
                 "K1",
                 "K1/SK1",
                 "K1/SK1/SSK1",
@@ -38,7 +38,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK1",
                 "K2/SK1/SSK2"
         );
-        List<String> expected = Arrays.asList(
+        List<String> expected = List.of(
                 "K2",
                 "K2/SK1",
                 "K2/SK1/SSK1",
